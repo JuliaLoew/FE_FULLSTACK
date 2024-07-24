4f1136f7b65b1be9e3d5b3ad4d447102b424b9f7
Henry Nguyen <henrylinh94@yahoo.de>
2024-07-24 17:28:57 +0200

    fixed bug

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,14 +3,14 @@ import {Link} from 'react-router-dom';
 import axios from 'axios';
 
 const Home = () => {
-  const [post, setPost] = useState([]);
+  const [posts, setPosts] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     axios
-    .get('http://localhost:3000/posts')
+    .get('http://localhost:3000')
     .then((response) => {
-      setPost(response.data);
+      setPosts(response.data);
     })
     .catch((error) => {
       setError(error.message);
@@ -22,30 +22,29 @@ const Home = () => {
       <div className='container mx-auto p-4'>
         <h1 className='text-3xl font-bold p-4'>All Blog Posts</h1>
         {error && <p className='text-red-500'>{error}</p>}
-        {post.map((post) => (
-          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
-            <div key={post.id} className='card bg-base-100 shadow-md'>
-              <figure>
-                <img src={post.cover} alt={post.title} />
-              </figure>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
+        {posts.map((posts) => (
+          <div key={posts.id} className='card bg-base-100 shadow-md'>
+            <figure>
+              <img src={posts.cover} alt={posts.title} />
+            </figure>
 
-              <div className='card-body'>
-                <h2 className='card-title'>
-                  {post.title}
-                </h2>
-                <p>
-                  {post.content.substring(0, 100)}...
-                </p>
-                <div className='card-actions justify-end'>
-                  <button className='btn btn-primary'>
-                    <Link to={`/posts/${post.id}`}>Read More</Link>
-                    </button>
-                </div>
+            <div className='card-body'>
+              <h2 className='card-title'>
+                {posts.title}
+              </h2>
+              <p>
+                {posts.content.substring(0, 100)}...
+              </p>
+              <div className='card-actions justify-end'>
+                <button className='btn btn-primary'>
+                  <Link to={`/posts/${posts.id}`}>Read More</Link>
+                  </button>
               </div>
-
             </div>
           </div>
         ))}
+        </div>
       </div>
     );
   };
